58a10ab46262ee9bd76a6acbd478f333bca97202
Demis Struiksma <contact@demisstruiksma.com>
2023-11-05 17:43:43 +0100

    Add shared types file

---
+++ src/app/types/sharedTypes.tsx
@@ -0,0 +1,12 @@
+export interface Movie {
+    id: number;
+    title: string;
+    overview: string;
+    release_date: string;
+    poster_path: string;
+    vote_average: number;
+}
+
+export interface Movies {
+    results: Movie[];
+}
