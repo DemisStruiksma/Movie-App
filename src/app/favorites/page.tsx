47b2f633daa3c0b04c53d102d3f135b8055ffea9
Demis Struiksma <contact@demisstruiksma.com>
2023-11-07 21:40:12 +0100

    Protect favorites route

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,10 +1,20 @@
 "use client"
 
 import MovieOverview from "@/components/organisms/MovieOverview";
+import { useSession, getSession } from "next-auth/react"
 
 export default function FavoritesPage() {
+    const { data: session, status } = useSession();
     const storedMovies = JSON.parse(localStorage.getItem('favoriteMovies') || '[]');
     const data = { results: storedMovies };
+
+    if (status === "loading") {
+        return <div>Loading...</div>
+    }
+
+    if (status === "unauthenticated") {
+        return <div>Access Denied</div>
+    }
 
     return (
         <MovieOverview data={data} />
