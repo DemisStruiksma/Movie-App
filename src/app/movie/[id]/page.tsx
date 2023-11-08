46c719aba41088fd69e527c2fcfc99ba7b3107da
Demis Struiksma <contact@demisstruiksma.com>
2023-11-08 15:48:11 +0100

    Use react query for movie details page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,38 +1,49 @@
+"use client"
+
 import Image from 'next/image'
+import { useQuery } from '@tanstack/react-query';
 import { Movie } from '@/app/types/sharedTypes';
 
-export default async function Page({params}: {
+export default function Page({params}: {
     params: {
         id: string;
     }
 }) {
     const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-    const res = await fetch(` https://api.themoviedb.org/3/movie/${params.id}?api_key=${apiKey}`)
-    const movie: Movie = await res.json();
+    const { data, isLoading } = useQuery<Movie>({
+        queryKey: ["movie"],
+        queryFn: () => fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${apiKey}`).then((res) => res.json()),
+    });
+
+    if (isLoading && !data) return <div>Loading...</div>;
 
     return (
-        <div className="flex flex-row items-start justify-center py-8">
-            <div className="w-1/2">
-                <Image
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                width={500}
-                height={500}
-                alt={movie.title}
-                className="rounded-lg"
-                />
-            </div>
-
-            <div className="w-1/2 p-4">
-                <h1 className="text-2xl font-semibold mb-2">{movie.title}</h1>
-                <div className="flex items-center mb-4">
-                    <span className="text-gray-500 mr-2">{movie.release_date}</span>
-                    
-                    <span className="bg-yellow-600 rounded-full text-white px-2 py-1 text-sm font-semibold">
-                        {movie.vote_average}
-                    </span>
+        <div className="flex flex-row items-start justify-center py-8">        
+            {data && (
+                <><div className="w-1/2">
+                    <Image
+                        src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                        width={500}
+                        height={500}
+                        alt={data.title}
+                        priority
+                        className="rounded-lg" />
                 </div>
-                <p className="text-gray-600 text-lg text-justify">{movie.overview}</p>
-            </div>
+                
+                <div className="w-1/2 p-4">
+                        <h1 className="text-2xl font-semibold mb-2">{data.title}</h1>
+
+                    <div className="flex items-center mb-4">
+                        <span className="text-gray-500 mr-2">{data.release_date}</span>
+
+                        <span className="bg-yellow-600 rounded-full text-white px-2 py-1 text-sm font-semibold">
+                            {data.vote_average}
+                        </span>
+                    </div>
+
+                    <p className="text-gray-600 text-lg text-justify">{data.overview}</p>
+                </div></>
+            )}
         </div>
     )
 }
